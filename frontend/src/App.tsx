a7851c4fba53b876d0a1f1467a3fdccc30b1f9fb
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-24 11:42:51 +0530

    fix: set default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Signup></Signup>}></Route>
           <Route path='/signup' element={<Signup></Signup>}></Route>
           <Route path='/signin' element={<Signin></Signin>}></Route>
           <Route path='/blogs' element={<Blogs></Blogs>}></Route>
